47e2185e2d3b4c799f575e98f5d72fa3094a0cb9
rio2828 <eddiekim1023@gmail.com>
2022-09-23 21:35:45 +0900

    MVC패턴 적용 후

--- RPSGame/Rps.swift
+++ RPSGame/Rps.swift
@@ -5,12 +5,29 @@
 //  Created by 김태현 on 2022/02/14.
 //
 
-import Foundation
-//원시값(raw value)
+import UIKit
+
+// 원시값(raw value)
 enum Rps: Int {
+    case ready
     case rock
     case paper
     case scissors
+    
+    // 계산 속성
+    var rpsInfo: (image: UIImage, name: String) {
+        switch self {
+        case .ready:
+            return (#imageLiteral(resourceName: "ready"),"준비")
+        case .rock:
+            return (#imageLiteral(resourceName: "rock"),"바위")
+        case .paper:
+            return (#imageLiteral(resourceName: "paper"),"보")
+        case .scissors:
+            return (#imageLiteral(resourceName: "scissors"),"가위")
+        }
+    }
+
 }
 
 
